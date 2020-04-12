7105e3b28c768bb9192c1b689da4628dde009040
kirthikka <38406867+kirthikka@users.noreply.github.com>
2020-04-12 22:03:17 +0530

    Update ReadTable_exe15_20.r

--- ReadTable_exe15_20.r
+++ ReadTable_exe15_20.r
@@ -1,2 +1,10 @@
 table_ele<- read.csv("hw1_data.csv")
 str(table_ele)
+
+
+myData <- read.csv(table_ele, skip =46, nrow = 47, ncol =0, skip=1)
+str (myData)
+
+
+myMissingVAL <- is.nan(table_ele)
+print (myMissingVAL)
